8f48d6c92b4a44822c68cebf137fa7e0d9df3695
Philippe Chambon <joan_krugg@live.fr>
2022-07-22 12:15:59 +0200

    millesime customers

--- app/models/millesime_customer.rb
+++ app/models/millesime_customer.rb
@@ -15,4 +15,21 @@ class MillesimeCustomer < ApplicationRecord
       end
     end
   end
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      Customer.where(mail: row[7]).first_or_create do |c|
+        c.genre = row[0]
+        c.name = row[1]
+        c.surname = row[2]
+        c.address = row[3]
+        c.city = row[4]
+        c.zipcode = row[5]
+        c.country = row[6]
+        c.phone = row[8]
+        c.birth = row[9]
+        c.home = row[10]
+        c.segment = row[11]
+      end
+    end
+  end
 end
